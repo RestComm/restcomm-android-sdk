d42ddd359a15b1efd0d0881d49544ce8e1874947
atsakiridis <antonis.tsakiridis@telestax.com>
2015-11-05 12:55:52 +0200

    Fixed #222: Rework the Settings screen to use new UI elements

--- Examples/restcomm-messenger/app/src/main/java/com/telestax/restcommmessenger/MainActivity.java
+++ Examples/restcomm-messenger/app/src/main/java/com/telestax/restcommmessenger/MainActivity.java
@@ -8,8 +8,6 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
--- Examples/restcomm-messenger/app/src/main/java/com/telestax/restcommmessenger/SettingsActivity.java
+++ Examples/restcomm-messenger/app/src/main/java/com/telestax/restcommmessenger/SettingsActivity.java
@@ -1,36 +1,32 @@
 package com.telestax.restcommmessenger;
 
-import android.R;
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-
-public class SettingsActivity extends AppCompatPreferenceActivity {
-//public class SettingsActivity extends PreferenceActivity {
-    @SuppressWarnings("deprecation")
+public class SettingsActivity extends AppCompatActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
 
-        setupActionBar();
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
 
-        // TODO: this is deprecated. The preferred way to go is to use preference fragment, as described at http://developer.android.com/guide/topics/ui/settings.html#Fragment
-        addPreferencesFromResource(com.telestax.restcommmessenger.R.xml.preferences);
-    }
-
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    private void setupActionBar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             // Show the Up button in the action bar.
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
     }
 
     @Override
@@ -47,4 +43,3 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     }
 
 }
-
