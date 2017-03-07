e774408208835ba1c23fcf285b8cc853e4ad20d2
Antonis <antonis.tsakiridis@telestax.com>
2017-03-07 13:55:24 +0200

    Fixed #487: Build Olympus App for deployment on real device and upload to Test Fairy

--- scripts/build-olympus.bash
+++ scripts/build-olympus.bash
@@ -22,8 +22,12 @@ echo "-- Installing provisioning profiles, so that XCode can find them"
 if [ ! -z "$TRAVIS" ]
 then
 	export ORG_GRADLE_PROJECT_VERSION_CODE=$TRAVIS_BUILD_NUMBER
-else
-	export ORG_GRADLE_PROJECT_VERSION_CODE=1
+fi
+
+if [ -z "$ORG_GRADLE_PROJECT_VERSION_CODE" ] || [ -z "$ORG_GRADLE_PROJECT_VERSION_NAME" ]
+then
+	echo "-- Error: ORG_GRADLE_PROJECT_VERSION_CODE and ORG_GRADLE_PROJECT_VERSION_NAME need to be set"
+	exit 1
 fi
 
 echo -e "-- Using versionName: $ORG_GRADLE_PROJECT_VERSION_NAME"
@@ -36,27 +40,27 @@ echo "-- Updating git commit hash for Olympus About screen"
 echo "-- Building Olympus and uploading to TestFairy"
 if [ -z "$SKIP_TF_UPLOAD" ] || [[ "$SKIP_TF_UPLOAD" == "false" ]]
 then
-	cd Examples/restcomm-olympus && ./gradlew assemble   # -PVERSION_CODE=$TRAVIS_BUILD_NUMBER -PVERSION_NAME=$VERSION_NAME
+	cd Examples/restcomm-olympus && ./gradlew -i -PtestfairyChangelog="Version: $ORG_GRADLE_PROJECT_VERSION_NAME+$ORG_GRADLE_PROJECT_VERSION_CODE, GitHub commit: $COMMIT_SHA1" testfairyDebug
 	cd ../..
 else
 	echo "-- Skipping upload to Test Fairy."
-	cd Examples/restcomm-olympus && ./gradlew -PtestfairyChangelog="Version: $ORG_GRADLE_PROJECT_VERSION_NAME+$ORG_GRADLE_PROJECT_VERSION_CODE, GitHub commit: $COMMIT_SHA1" testfairyDebug
+	cd Examples/restcomm-olympus && ./gradlew assemble   # -PVERSION_CODE=$TRAVIS_BUILD_NUMBER -PVERSION_NAME=$VERSION_NAME
 	cd ../..
 fi
 
 # Clean up
-echo "-- Cleaning up"
+#echo "-- Cleaning up"
 
-echo "-- Deintegrating olympus pods"
+#echo "-- Deintegrating olympus pods"
 
-echo "-- Rolling back changes in source files: $SDK_COMMON_HEADER, $OLYMPUS_UTILS, $OLYMPUS_PLIST"
+#echo "-- Rolling back changes in source files: $SDK_COMMON_HEADER, $OLYMPUS_UTILS, $OLYMPUS_PLIST"
 #git checkout -- $SDK_COMMON_HEADER $OLYMPUS_UTILS $OLYMPUS_PLIST $OLYMPUS_APP_DELEGATE
 
-echo "-- Setting original keychain, \"$ORIGINAL_KEYCHAIN\", as default"
+#echo "-- Setting original keychain, \"$ORIGINAL_KEYCHAIN\", as default"
 #security default-keychain -s $ORIGINAL_KEYCHAIN
 
-echo "-- Removing custom keychain $CUSTOM_KEYCHAIN"
+#echo "-- Removing custom keychain $CUSTOM_KEYCHAIN"
 #security delete-keychain $CUSTOM_KEYCHAIN
 
-echo "-- Removing keys, certs and profiles"
+#echo "-- Removing keys, certs and profiles"
 #rm scripts/certs/${DEVELOPMENT_CERT} 
