e8edb26d62766d2453533b327d1f5f55a5823865
Antonis <antonis.tsakiridis@telestax.com>
2017-03-23 15:42:34 +0200

    Working on #491: Upload SDK to sonatype from CI/CD facilities: fixed some script issues

--- scripts/publish-sdk.bash
+++ scripts/publish-sdk.bash
@@ -9,6 +9,11 @@ then
 	echo "-- Decrypting and installing global gradle.properties"
 	openssl aes-256-cbc -k "$FILE_ENCRYPTION_PASSWORD" -in scripts/configuration/${GLOBAL_GRADLE_PROPERTIES}.enc -d -a -out scripts/configuration/${GLOBAL_GRADLE_PROPERTIES}
   	cp scripts/configuration/${GLOBAL_GRADLE_PROPERTIES} ~/.gradle/${GLOBAL_GRADLE_PROPERTIES} 
+
+	echo "-- Decrypting and installing gnupg resources"
+	ls ~/.gnupg
+	openssl aes-256-cbc -k "$FILE_ENCRYPTION_PASSWORD" -in scripts/configuration/${GPG_SECRING}.enc -d -a -out scripts/configuration/${GPG_SECRING}
+  	cp scripts/configuration/${GPG_SECRING} ~/.gnupg/
 fi
 
 echo "-- Building SDK and uploading to maven repository"
