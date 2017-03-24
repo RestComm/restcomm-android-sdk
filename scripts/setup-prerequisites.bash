ee6c78ce1939334e7233a82dac6f3b424b63d4c3
Antonis <antonis.tsakiridis@telestax.com>
2017-03-24 17:25:54 +0200

    Working on #491: Upload SDK to sonatype from CI/CD facilities: fixed doc generation

--- scripts/setup-prerequisites.bash
+++ scripts/setup-prerequisites.bash
@@ -9,14 +9,14 @@ then
 
 	echo "-- Decrypting and installing global gradle.properties"
 	openssl aes-256-cbc -k "$FILE_ENCRYPTION_PASSWORD" -in scripts/configuration/${GLOBAL_GRADLE_PROPERTIES}.enc -d -a -out scripts/configuration/${GLOBAL_GRADLE_PROPERTIES}
-  	cp scripts/configuration/${GLOBAL_GRADLE_PROPERTIES} ~/.gradle/${GLOBAL_GRADLE_PROPERTIES} 
+  	mv scripts/configuration/${GLOBAL_GRADLE_PROPERTIES} ~/.gradle/${GLOBAL_GRADLE_PROPERTIES} 
 
 	echo "-- Decrypting and installing gnupg resources"
 	ls ~/.gnupg
 	openssl aes-256-cbc -k "$FILE_ENCRYPTION_PASSWORD" -in scripts/configuration/${GPG_SECRING}.enc -d -a -out scripts/configuration/${GPG_SECRING}
-  	cp scripts/configuration/${GPG_SECRING} ~/.gnupg/
+  	mv scripts/configuration/${GPG_SECRING} ~/.gnupg/
 
 	openssl aes-256-cbc -k "$FILE_ENCRYPTION_PASSWORD" -in scripts/configuration/${GPG_TRUSTDB}.enc -d -a -out scripts/configuration/${GPG_TRUSTDB}
-  	cp scripts/configuration/${GPG_TRUSTDB} ~/.gnupg/
+  	mv scripts/configuration/${GPG_TRUSTDB} ~/.gnupg/
 fi
 
